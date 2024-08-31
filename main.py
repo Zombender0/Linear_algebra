bb4d0b39a744e5d85ba9c2cc429381c5cce855bf
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-08-31 00:20:10 -0600

    Mejoras

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from models.GaussJordan import GaussJordan 
-from models.Matrix import Matrix
-
 
 matrix = [[1,4,-2,8,12],[0,1,-7,2,-4],[0,0,5,-1,7],[0,0,1,3,-5]]
+if not GaussJordan._valid_matrix(matrix):
+    exit(0)
 m = GaussJordan(matrix)
-print(m)
-
-        
+m.gauss_jordan()
--- models/Matrix.py
+++ models/Matrix.py
@@ -5,24 +5,45 @@ class Matrix():
         self.width = len(matrix)
         self.length = len(matrix[0])
     
-    def __valid_matrix(self):
-        pass
-    def swap(self,row,row_to_swap,print=True) -> None:
+    @staticmethod
+    def _valid_matrix(matrix: list[list]) -> bool:
+        width = len(matrix)
+        length = len(matrix[0])
+        zero_row = True
+        for row in range(width):
+            zero_row = True
+            for col in range(length):
+                if matrix [row][col] !=0: zero_row = False
+                if isinstance(matrix[row][col],float):
+                    matrix[row][col] = int(matrix[row][col])
+                    continue
+                if isinstance(matrix[row][col], str):
+                    print('Valor no numérico encontrado')
+                    return False
+                if len(matrix[row]) !=length:
+                    print('Matriz incompleta')
+                    return False
+            if zero_row:
+                print('Matriz inconsistente')
+                return False
+        return True
+
+    def swap(self,row,row_to_swap,print_=True) -> None:
         '''
         Permite cambiar entre dos filas de una matriz. 
         
         Parámetros:
         row (int) : Una de las filas que se va a intercambiar
         row_to_swap (int) : La otra fila que se va a intercambiar
-        print (bool) : Determina si se debe imprimir el cambio realizado con la matriz. Por defecto es verdadero
+        print_ (bool) : Determina si se debe imprimir el cambio realizado con la matriz. Por defecto es verdadero
 
         El cambio se realiza en la matriz de la instancia que se esté trabajando.
         '''
 
         self.matrix[row],self.matrix[row_to_swap] = self.matrix[row_to_swap],self.matrix[row]
-        if print: 
+        if print_: 
             print(f"f{row+1} <--> f{row_to_swap+1}\n")
-            self
+            print(self)
 
     @staticmethod
     def gcd(a: int,b: int) -> int: 
@@ -52,6 +73,6 @@ class Matrix():
         rep = ''
         for row in range(self.width):
             for col in range(self.length):
-                rep+=f'{self.matrix[row][col]} '
+                rep+=f'{int(self.matrix[row][col])} '
             rep +='\n'
         return rep
