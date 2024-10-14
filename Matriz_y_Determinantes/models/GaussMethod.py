441b0c6024c9d0088b5529637f05b08229501717
Karen Fonseca <127228701+littlesaturnalien@users.noreply.github.com>
2024-10-14 14:54:16 -0600

    Pasos de GaussMethod guardados en diccionario

--- Matriz_y_Determinantes/models/GaussMethod.py
+++ Matriz_y_Determinantes/models/GaussMethod.py
@@ -1,9 +1,11 @@
 from models.Matrix import Matrix
+import copy
 
 class GaussMethod(Matrix):
     def __init__(self, matriz: list[list]) -> None:
         super().__init__(matriz)
         self.det = 1 #Determinante inicializado
+        self.config = {}
     
     def gauss_method(self):
         if not self.determinante_nulo():
@@ -11,24 +13,26 @@ class GaussMethod(Matrix):
                 self.pivote_triangular(col)
                 self.reduccion_triangular(col)
             self.calculo_determinante()
+        
+        return self.config
     
     def determinante_nulo(self) -> bool:
         #Validación por si una matriz tiene líneas de ceros.
         for fila in range(self.filas):
             if all(self.matriz[fila][i] == 0 for i in range(self.columnas)):
-                print("\nLa fila " + str(fila+1) + " es una línea de ceros. Por lo tanto, el determinante es igual a 0")
+                self.config[f"\nLa fila {fila + 1} es una línea de ceros. Por lo tanto, el determinante es igual a 0"] = copy.deepcopy(self.matriz)
                 return True
 
         for col in range(self.columnas):
             if all(self.matriz[i][col] == 0 for i in range(self.filas)):
-                print("\nLa columna " + str(col+1) + " es una línea de ceros. Por lo tanto, el determinante es igual a 0")
+                self.config[f"\nLa columna {col + 1} es una línea de ceros. Por lo tanto, el determinante es igual a 0"] = copy.deepcopy(self.matriz)
                 return True
         
         #Validación en caso de que una matriz tenga dos filas o dos columnas iguales.
         for i in range(self.filas):
             for j in range(i + 1, self.filas):
                 if self.matriz[i] == self.matriz[j]:
-                    print(f"\nLas filas {i+1} y {j+1} son iguales. Por lo tanto, el determinante es igual a 0.")
+                    self.config[f"\nLas filas {i+1} y {j+1} son iguales. Por lo tanto, el determinante es igual a 0."] = copy.deepcopy(self.matriz)
                     return True
 
         for i in range(self.columnas):
@@ -36,21 +40,20 @@ class GaussMethod(Matrix):
                 col_i = [self.matriz[k][i] for k in range(self.filas)]
                 col_j = [self.matriz[k][j] for k in range(self.filas)]
                 if col_i == col_j:
-                    print(f"\nLas columnas {i+1} y {j+1} son iguales. Por lo tanto, el determinante es igual a 0.")
+                    self.config[f"\nLas columnas {i+1} y {j+1} son iguales. Por lo tanto, el determinante es igual a 0."] = copy.deepcopy(self.matriz)
                     return True
         
         return False
     
     def calculo_determinante(self):
-        exp = "DET A = "
+        exp = "Determinante = "
         operacion = f"({self.det}) "
         for i in range(self.filas):
             operacion += f"({self.matriz[i][i]:.1f})"
             self.det *= self.matriz[i][i]
         
         respuesta = exp + operacion
-        print(respuesta)
-        print(f"DET A = {self.det}")
+        self.config[respuesta] = (copy.deepcopy(self.matriz), f'Determinante = {self.det}')
     
     def pivote_triangular(self, col: int):
         mejor_fila = -1
@@ -85,28 +88,22 @@ class GaussMethod(Matrix):
                         mejor_columna = col_alt
         
         if mejor_fila == -1:
-            print(f"\nNo se encontró pivote en la columna {col + 1}.\n")
-            print(self)
-            print(f" DET A = {self.det}\n\n")
+            self.config[f"\nNo se encontró pivote en la columna {col + 1}.\n"] = (copy.deepcopy(self.matriz), f'Determinante = {self.det}')
             return
         
         if mejor_columna != col:
             self.intercambio_col(col, mejor_columna)
-            self.det = self.det * -1
-            print(f" DET A = {self.det}\n\n")
             return
         
         if mejor_fila != col:
             self.intercambio(col, mejor_fila)
-            self.det = self.det * -1
-            print(f" DET A = {self.det}\n\n")
             return
     
     def intercambio_col(self, col, columna_intercambio) -> None:
         for fila in range(self.filas):
             self.matriz[fila][col],self.matriz[fila][columna_intercambio] = self.matriz[fila][columna_intercambio],self.matriz[fila][col]
-        print(f"\nC{col + 1} <--> C{columna_intercambio + 1}\n")
-        print(self)
+        self.det = self.det * -1
+        self.config[f"\nC{col + 1} <--> C{columna_intercambio + 1}\n"] = (copy.deepcopy(self.matriz), f'Determinante = {self.det}')
 
 
     def intercambio(self, fila, fila_intercambio) -> None:
@@ -114,8 +111,8 @@ class GaussMethod(Matrix):
         Tiene como parámetros el índice de la fila sin pivote y el índice de la fila con pivote para intercambiarlas.'''
 
         self.matriz[fila],self.matriz[fila_intercambio] = self.matriz[fila_intercambio],self.matriz[fila]
-        print(f"\nF{fila + 1} <--> F{fila_intercambio + 1}\n")
-        print(self)
+        self.det = self.det * -1
+        self.config[f"\nF{fila + 1} <--> F{fila_intercambio + 1}\n"] = (copy.deepcopy(self.matriz), f'Determinante = {self.det}')
     
 
     def reduccion_triangular(self, col: int):
@@ -142,12 +139,10 @@ class GaussMethod(Matrix):
             dividendo_tipo = int(abs(dividendo)) if abs(dividendo).is_integer() else f"{abs(dividendo):.1f}"
 
             if abs(divisor) == 1:
-                print(f"\nF{fila + 1} -> F{fila + 1} {operador} {dividendo_tipo}F{col + 1}\n")
+                self.config[f"\nF{fila + 1} -> F{fila + 1} {operador} {dividendo_tipo}F{col + 1}\n"] = (copy.deepcopy(self.matriz), f'Determinante = {self.det}')
             else:
                 divisor_tipo = int(abs(divisor)) if abs(divisor).is_integer() else f"{abs(divisor):.1f}"
-                print(f"\nF{fila + 1} -> F{fila + 1} {operador} ({dividendo_tipo}/{divisor_tipo})F{col + 1}\n")
-            print(self)
-            print(f" DET A = {self.det}\n\n")
+                self.config[f"\nF{fila + 1} -> F{fila + 1} {operador} ({dividendo_tipo}/{divisor_tipo})F{col + 1}\n"] = (copy.deepcopy(self.matriz), f'Determinante = {self.det}')
 
 
     
