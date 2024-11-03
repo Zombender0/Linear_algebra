ac4159787a34f77535ae6dfb0fcd280a169fa6c0
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-11-02 18:37:46 -0600

    la variable de entorno en main fue eliminada y reemplazada. Ver README.md

--- app/main.py
+++ app/main.py
@@ -1,17 +1,4 @@
 import sys 
-import os 
-
-PYTHONPATH = 'algebra_path'
-directory_path = os.getenv(PYTHONPATH)
-
-if directory_path:
-    sys.path.append(directory_path)
-else:
-    current_path = os.path.abspath(__file__)
-    dir_path = os.path.dirname(current_path)
-    parent_path = os.path.dirname(dir_path)
-    sys.path.append(parent_path)
-
 from PySide6.QtWidgets import QMainWindow, QApplication
 from controller.main_controller import MainController
 
