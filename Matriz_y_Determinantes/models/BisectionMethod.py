1d18239e5fd76a330de2fbdf076f2e235bea5851
Karen Fonseca <127228701+littlesaturnalien@users.noreply.github.com>
2024-11-03 20:53:35 -0600

    Método de bisección agregado

---
+++ Matriz_y_Determinantes/models/BisectionMethod.py
@@ -0,0 +1,77 @@
+import copy
+from EquationEvaluator import EquationEvaluator
+
+class BisectionMethod():
+    def __init__(self, f, a, b, tol) -> None:
+        if not abs(tol) <= 1:
+            raise ValueError("La tolerancia debe ser un porcentaje entre 0 y 1.")
+        
+        self.funcion = f
+        self.interval_a = a
+        self.interval_b = b
+        self.media_c = None
+        self.n_iter = 0
+        self.tolerancia = abs(tol)
+        self.datos_por_iteracion = []
+        self.config = {}
+    
+    def bisection_method(self):
+
+        fa = EquationEvaluator(self.funcion, self.interval_a)
+        fb = EquationEvaluator(self.funcion, self.interval_b)
+        
+        if fa * fb >= 0:
+            print("La función debe cambiar de signo en el intervalo dado [a, b].")
+            return
+        
+        c_anterior = None
+        while True:
+            self.n_iter += 1
+            self.media_c = (self.interval_a + self.interval_b)/2
+            fc = EquationEvaluator(self.funcion, self.media_c)
+
+            if c_anterior is not None:
+                error_porcentual = abs(((self.media_c - c_anterior)/self.media_c)*100)
+                if error_porcentual/100 <= self.tolerancia:
+                    self.datos_por_iteracion.append([self.interval_a, self.interval_b, self.media_c, fa, fb, fc, fa * fc, error_porcentual])
+                    print(f"La raíz aproximada es {self.media_c} con un error porcentual de {error_porcentual}%")
+                    print(f"Número de iteraciones: {self.n_iter}\n")
+                    break
+            else:
+                error_porcentual = "-"
+            
+            self.datos_por_iteracion.append([self.interval_a, self.interval_b, self.media_c, fa, fb, fc, fa*fc, error_porcentual])
+            
+            if fc == 0:
+                self.datos_por_iteracion.append([self.interval_a, self.interval_b, self.media_c, fa, fb, fc, fa * fc, error_porcentual])
+                print(f"La raíz aproximada es {self.media_c}.")
+                print(f"Número de iteraciones: {self.n_iter}\n")
+                break
+
+        
+            if fa*fc > 0:
+                self.interval_a = self.media_c
+                fa = EquationEvaluator(self.funcion, self.interval_a)
+            else:
+                self.interval_b = self.media_c
+                fb = EquationEvaluator(self.funcion, self.interval_b)
+            
+            c_anterior = self.media_c
+
+        self.print_iterations()
+
+    def print_iterations(self):
+        """
+        Imprime los datos de cada iteración en formato de tabla.
+        """
+        headers = ["intervalo a", "intervalo b", "media c", "fa", "fb", "fa*fc", "error porcentual"]
+        print(f"{' | '.join(headers)}")
+        print("-" * 90)
+        for row in self.datos_por_iteracion:
+            print(" | ".join(f"{value:.6f}" if isinstance(value, float) else str(value) for value in row))
+
+
+if __name__ == '__main__':
+    BM = BisectionMethod('(x**3)-sen(2*x)+(e**x)', -1.5, 2, 0.01)
+    BM.bisection_method()
+
---
+++ Matriz_y_Determinantes/models/EquationEvaluator.py
@@ -0,0 +1,14 @@
+from constants.math_functions import ALLOWED_DATA
+from copy import deepcopy
+
+def EquationEvaluator(equation:str,x_value:int|float)->float|bool:
+    allowed_data_copy = deepcopy(ALLOWED_DATA)
+    allowed_data_copy['x'] = x_value
+    try:
+        result = eval(equation, {"__builtins__": None}, allowed_data_copy)
+    except Exception as e:
+        return None
+    return result
+
+if __name__ == '__main__':
+    print(EquationEvaluator('(x**3)-sin(2*x)+(e**x)', 0))
