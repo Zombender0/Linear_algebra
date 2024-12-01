8c37bce83b551431a7c0986393e88fd35c5dd64f
Karen Fonseca <127228701+littlesaturnalien@users.noreply.github.com>
2024-12-01 01:19:41 -0600

    Matriz inversa se puede resolver con fraccion

--- Matriz_y_Determinantes/models/InvertibleMatrix.py
+++ Matriz_y_Determinantes/models/InvertibleMatrix.py
@@ -1,43 +1,48 @@
 import copy
-from models.Matrix import Matrix
-from models.GaussMethod import GaussMethod
+from Matrix import Matrix
+from GaussMethod import GaussMethod
+from fractions import Fraction
 
 class InvertibleMatrix(Matrix):
-    def __init__(self, matriz: list[list]) -> None:
-        super().__init__(matriz)
+    def __init__(self, matriz: list[list], fraccion:bool) -> None:
+        super().__init__(matriz, fraccion)
         self.config = {}
-        self.matriz_identidad = [[1.0 if i == j else 0.0 for j in range(self.filas)] for i in range(self.filas)]
+        self.identidad_valor_uno = Fraction(1) if self.fraccion_oper else 1.0
+        self.identidad_valor_cero = Fraction(0) if self.fraccion_oper else 0.0
+
+        self.matriz_identidad = [
+            [self.identidad_valor_uno if i == j else self.identidad_valor_cero for j in range(self.filas)] 
+            for i in range(self.filas)]
+        
         self.matriz_aumentada = [self.matriz[i] + self.matriz_identidad[i] for i in range(self.filas)]
 
     def invertibleMatrix(self):
         if self.filas != self.columnas:
-            print("La matriz debe ser CUADRADA 'n x n'")
-            return #VALIDAR DIRECTAMENTE DESDE LA INTERFAZ
+            return False
         
         if self.singularMatrix():
-            print("El determminante es igual a 0, por lo tanto, la matriz A no tiene inversa")
-            return
-
-        print("MATRIZ AUMENTADA")
-        self.imprimir_matriz_aumentada()
-
+            self.config["El determinante es igual a 0, por lo tanto, la matriz A no tiene inversa"] = copy.deepcopy(self.matriz)
+            return self.config
+        
+        self.config["Matriz Aumentada"] = copy.deepcopy(self.matriz_aumentada)
+        #self.imprimir_matriz_aumentada()
         self.gauss_jordan_inversa()
+        return self.config
 
         
     def gauss_jordan_inversa(self):
         for col in range(self.columnas):
             if self.convertir_a_1(col):
                 self.reduccion_a_cero(col)
-        
-        if all(self.matriz_aumentada[i][j] == (1.0 if i == j else 0.0) for i in range(self.filas) for j in range(self.columnas)):
+        if all(self.matriz_aumentada[i][j] == (self.identidad_valor_uno if i == j else self.identidad_valor_cero) 
+               for i in range(self.filas) for j in range(self.columnas)):
             self.solucion_matriz_inversa()
         else:
-            print("No hay solución en Gauss-Jordan, por lo tanto, la matriz es singular.")
+            self.config["No hay solucion en Gauss-Jordan, por lo tanto, la matriz es singular."] = copy.deepcopy(self.matriz_aumentada)
         
     def solucion_matriz_inversa(self):
         matriz_inversa = [fila[self.columnas:] for fila in self.matriz_aumentada]
-        print("\nMATRIZ INVERSA")
-        self.imprimir_matriz(matriz_inversa)
+        self.config['Matriz inversa'] = copy.deepcopy(matriz_inversa)
      
     def convertir_a_1(self, col: int) -> bool:
         if self.matriz_aumentada[col][col] == 0:
@@ -50,15 +55,16 @@ class InvertibleMatrix(Matrix):
 
         if pivote != 1 and pivote != 0:
             self.matriz_aumentada[col] = [x / pivote for x in self.matriz_aumentada[col]]
-            print(f"\nF{col + 1} -> F{col + 1} / {int(pivote) if pivote.is_integer() else f'{pivote:.2f}'}\n")
-            self.imprimir_matriz_aumentada()
+            if not self.fraccion_oper:
+                self.config[f"F{col + 1} -> F{col + 1} / {int(pivote) if pivote.is_integer() else f'{pivote:.2f}'}"] = copy.deepcopy(self.matriz_aumentada)
+            else:
+                self.config[f"F{col + 1} -> F{col + 1} / {pivote}"] = copy.deepcopy(self.matriz_aumentada)
         
         return True if self.matriz_aumentada[col][col] == 1 else False
         
     def intercambio(self, fila, fila_intercambio) -> None:
         self.matriz_aumentada[fila],self.matriz_aumentada[fila_intercambio] = self.matriz_aumentada[fila_intercambio],self.matriz_aumentada[fila]
-        print(f"\nF{fila + 1} <--> F{fila_intercambio + 1}\n")
-        self.imprimir_matriz_aumentada()
+        self.config[f"F{fila + 1} <--> F{fila_intercambio + 1}"] = copy.deepcopy(self.matriz_aumentada)
 
     def reduccion_a_cero(self, col : int):
         for fila in range(self.filas):
@@ -73,33 +79,34 @@ class InvertibleMatrix(Matrix):
                 operador = "-"
                 operando = -operando
 
-            operando_tipo = int(operando) if operando.is_integer() else f"{operando:.2f}"
-
-            print(f"\nF{fila + 1} -> F{fila + 1} {operador} {operando_tipo}F{col + 1}\n")
-            self.imprimir_matriz_aumentada()
+            if not self.fraccion_oper:
+                operando_tipo = int(operando) if operando.is_integer() else f"{operando:.2f}"
+                self.config[f"F{fila + 1} -> F{fila + 1} {operador} {operando_tipo}F{col + 1}"] = copy.deepcopy(self.matriz_aumentada)
+            else:
+                self.config[f"F{fila + 1} -> F{fila + 1} {operador} {operando}F{col + 1}"] = copy.deepcopy(self.matriz_aumentada)
 
     def singularMatrix(self) -> bool:
         matriz_cuadrada_copia = copy.deepcopy(self.matriz)
-        matrizA = GaussMethod(matriz_cuadrada_copia)
-
-        print("MATRIZ A")
-        self.imprimir_matriz(matrizA.matriz)
+        matrizA = GaussMethod(matriz_cuadrada_copia, self.fraccion_oper)
+        #Continue here
+        #self.config["Matriz A"] = copy.deepcopy(matriz)
+        #print("MATRIZ A")
+        #self.imprimir_matriz(matrizA.matriz)
 
         matrizA.gauss_method()
 
-        print("MATRIZ A EN FORMA TRIANGULAR (Y SU DETERMINANTE)")
-        self.imprimir_matriz(matrizA.matriz)
+        #print("MATRIZ A EN FORMA TRIANGULAR (Y SU DETERMINANTE)")
+        #self.imprimir_matriz(matrizA.matriz)
         determinante_sistema = matrizA.det
-        print(f"Det A = {determinante_sistema}\n")
+        #print(f"Det A = {determinante_sistema}\n")
 
         if determinante_sistema == 0:
             return True
-        
         return False
 
     def imprimir_matriz(self, matriz_cualquiera):
         matriz : str = ''
-        maximo_tamaño_fila = max(len(f'{round(matriz_cualquiera[fila][columna], 3)}') for fila in range(len(matriz_cualquiera)) for columna in range(len(matriz_cualquiera[0]))) + 2
+        maximo_tamaño_fila = max(len(f'{round(matriz_cualquiera[fila][columna], 4)}') for fila in range(len(matriz_cualquiera)) for columna in range(len(matriz_cualquiera[0]))) + 2
         for fila in range(len(matriz_cualquiera)):
             fila_str = ''
             for columna in range(len(matriz_cualquiera[0])):
@@ -109,11 +116,11 @@ class InvertibleMatrix(Matrix):
 
     def imprimir_matriz_aumentada(self):
         matriz : str = ''
-        maximo_tamaño_fila = max(len(f'{round(self.matriz_aumentada[fila][columna], 3)}') for fila in range(self.filas) for columna in range(len(self.matriz_aumentada[0]))) + 2
+        maximo_tamaño_fila = max(len(f'{round(self.matriz_aumentada[fila][columna], 4)}') for fila in range(self.filas) for columna in range(len(self.matriz_aumentada[0]))) + 2
         for fila in range(self.filas):
             fila_str = ''
             for columna in range(len(self.matriz_aumentada[0])):
-                fila_str += f'{round(self.matriz_aumentada[fila][columna], 3):<{maximo_tamaño_fila}}'
+                fila_str += f'{round(self.matriz_aumentada[fila][columna], 4):<{maximo_tamaño_fila}}'
             matriz += fila_str + '\n'
         print(matriz)
     
