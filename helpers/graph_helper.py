f0df4035ca67a05ec3618b456cb308626133d932
Johan Reyes <142470650+Zombender@users.noreply.github.com>
2024-12-02 19:20:44 -0600

    Cambio en la clase que maneja las gráficas, ahora admite valores grandes

--- helpers/graph_helper.py
+++ helpers/graph_helper.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 import pyqtgraph as pg
 from math import cos
 from models.EquationFunctions import EquationEvaluator
-
+from numpy import linspace
 class PyQtGraphWidget(QWidget):
     def __init__(self,parent=None):
         super().__init__(parent)
@@ -12,12 +12,17 @@ class PyQtGraphWidget(QWidget):
         self.plot_widget = pg.PlotWidget()
         self.layout.addWidget(self.plot_widget)
         self.plot_widget.setBackground('#2c313c')
-
+        self.stop = 100
+        self.num_points = 1000
     def paint_equation(self,equation):
         self.plot_widget.clear()
-        if EquationEvaluator(equation,1) is None:
+        
+        if EquationEvaluator(equation, 1) is None:
             print('ERROR')
             return None
-        x = [number for number in range(-100 if EquationEvaluator(equation, -1) is not None else 0, 100)] 
-        y = [EquationEvaluator(equation,number) for number in x]
-        self.plot_widget.plot(x,y,pen=pg.mkPen(color='#ec134e',width=5),symbol='o',symbolBrush='b')
+        start = -100 if EquationEvaluator(equation, -1) is not None else 0
+        
+        x = linspace(start, self.stop, self.num_points)
+        
+        y = [EquationEvaluator(equation, number) for number in x]
+        self.plot_widget.plot(x, y, pen=pg.mkPen(color='#ec134e', width=5), symbol='o', symbolBrush='b')
